206e4e03a37f394f53729d24dcd77e7a037d8d6a
seshyweshyy <179556178+seshyweshyy@users.noreply.github.com>
2026-04-14 00:10:55 +1000

    song title and author placeholder change

--- boringNotch/models/PlaybackState.swift
+++ boringNotch/models/PlaybackState.swift
@@ -16,9 +16,9 @@ enum RepeatMode: Int, Codable {
 struct PlaybackState {
     var bundleIdentifier: String
     var isPlaying: Bool = false
-    var title: String = "I'm Handsome"
-    var artist: String = "Me"
-    var album: String = "Self Love"
+    var title: String = "Not Playing"
+    var artist: String = "Unknown"
+    var album: String = "Unknown"
     var currentTime: Double = 0
     var duration: Double = 0
     var playbackRate: Double = 1
